68cf06470d38740e06895fe8299ce05e0953e69e
RahasoftBwire <bsccs202254233@mylife.mku.ac.ke>
2025-07-11 11:32:03 +0300

    Add email-validator to requirements for Render

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from datetime import timedelta
 class Config:
     """Base configuration class"""
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev-secret-key-change-in-production'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///chamalink.db'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     
     # M-Pesa Configuration
